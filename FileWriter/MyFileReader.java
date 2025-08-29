12ab2f966ad58e3838d0f22305a74b46163d6171
danielbaek <dbaek1@hwemail.com>
2025-08-29 08:53:02 -0700

    FileWriter initialization
    FleWriter now works

---
+++ FileWriter/MyFileReader.java
@@ -0,0 +1,9 @@
+package FileWriter;
+
+public class MyFileReader {
+    public static void main(String[] args) throws Exception {
+        try {
+            if(1 != 0) throw new Exception("lol");
+        }
+    }
+}
